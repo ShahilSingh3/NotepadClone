9b1b5f1ad5d13907de013ddcea51edb1ce86f786
Krishna-Mutha <krishna.mutha100@gmail.com>
2023-07-17 21:32:19 +0530

    git commit -m "Uploaded code"

---
+++ notepadclone.py
@@ -0,0 +1,14 @@
+from tkinter import *
+r=Tk()
+def text_adjust(event):
+    global textarea
+    main_geometry=r.geometry()
+    main_geometry=main_geometry.split('+')
+    main_geometry=main_geometry[0]
+    main_geometry=main_geometry.split('x')
+    textarea.config(width=main_geometry[0],height=main_geometry[1])
+r.bind('<Configure>',text_adjust)
+textarea=Text(r,borderwidth=0)
+textarea.pack()
+r.geometry('900x500')
+r.mainloop()
